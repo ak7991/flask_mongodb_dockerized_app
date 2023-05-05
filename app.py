ccf8083944393261eb0402c2a1a02a9ba87ee276
Akshay Alok <akshay.alok@myndsol.com>
2023-05-05 18:47:26 +0530

    Fix order id fetching

--- app.py
+++ app.py
@@ -1,53 +1,59 @@
 from collections import defaultdict
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pymongo
 from pymongo import MongoClient
 
+DATABASE_NAME = "orders_db"
+
 app = Flask(__name__)
 
-def get_db():
-    client = MongoClient(host='test_mongodb',
+def get_db(db_name=DATABASE_NAME):
+    client = MongoClient(host='localhost',
                          port=27017, 
                          username='root', 
                          password='pass',
-                        authSource="admin")
-    db = client["animal_db"]
+                         authSource="admin")
+    db = client[db_name]
     return db
 
 @app.route('/ignition')
 def ping_server():
     return "The api is working fine."
 
-@app.route('/animals')
-def get_stored_animals():
-    db=""
+@app.route('/orders')
+def get_all_orders():
+    db = ""
     try:
         db = get_db()
-        _animals = db.animal_tb.find()
-        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-        return jsonify({"animals": animals})
+        cursor = db.orders_tb.find()
+        data = list(cursor)
+        res = []
+        print("Debugging all orders data: ", data)
+        for i in data:
+            i.pop("_id")
+            res.append(i)
+        return jsonify({"all_orders": res})
     except:
         pass
     finally:
-        if type(db)==MongoClient:
+        if type(db) == MongoClient:
             db.close()
 
+
 @app.route('/order_id')
-def get_orders(order_id):
+def get_orders():
     db=""
     try:
         db = get_db()
-        cursor = db.orders_tb.find({"id": order_id})
+        order_id = int(request.args.get('order_id'))
+        cursor = db.orders_tb.find({"order_id": order_id})
         result = []
         for element in cursor:
-            temp = {
-                "order_id": element.order_id,
-                "product_count": element.product_count,
-                "products": element.products
-                }
-            result.append(temp)
+            element.pop("_id")
+            result.append(element)
         
+        print("Debugging result: ", result)
         return jsonify({"result": result})
     except:
         pass
@@ -65,6 +71,7 @@ def get_avg_products():
         for element in cursor:
             count += 1
             num += len(element.products)
+        print("Debugging avg endpoint: ", num, count)
         return jsonify({"average_products": num / count})
     except:
         pass
@@ -86,6 +93,7 @@ def get_avg_quantity(product_id):
                 nums[product.id][0] += product.quantity
                 nums[product.id][1] += 1
                 nums[product.id][2] = nums[product.id][0] / nums[product.id][1]
+        print("Debugging avg qty endpoint: ", nums)
 
         return jsonify({"avg_qty_per_product": nums})
     except:
